5c062210ffea72bad11e70f4752a6e53ecdba978
Lebcir Salah <slebcir@2D-LINUX-011.insa-lyon.fr>
2025-05-12 21:42:21 +0200

    Forcer la recréation de la base avec la colonne 'cible'

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def get_comments():
 # Lancement du serveur
 if __name__ == "__main__":
     with app.app_context():
-        db.drop_all()        
+        db.drop_all()
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
 
