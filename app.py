51a01a0a66d97ecc51e1e533768389cc2ffe7c52
Lebcir Salah <slebcir@2D-LINUX-011.insa-lyon.fr>
2025-05-12 21:24:13 +0200

    Drop + recreate DB to add colonne cible

--- app.py
+++ app.py
@@ -129,6 +129,7 @@ def get_comments():
 # Lancement du serveur
 if __name__ == "__main__":
     with app.app_context():
+        db.drop_all()        
         db.create_all()
     app.run(debug=True)
 
